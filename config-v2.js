5e4dfc1c482d9daaffdbdcfca8f8cc0ba13e0867
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-12-30 07:41:29 +0200

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "265994565557"
+const SESSION_ID = "PRABATH-MD~1Vx1EaCS#oMeggQqOaltHnu5wuVyN9SVmkjpSsDhyOlrNzNS4uUQ"
 
 
 module.exports = {
